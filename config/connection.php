5cd8d2260e0c272dcd9f98cb0657dddccb8c2339
tomica95 <45568534+tomica95@users.noreply.github.com>
2019-05-29 00:10:50 +0200

    config&connection&env

---
+++ config/connection.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once "config.php";
+try {
+    
+    
+
+    $conn = new PDO("mysql:host=".SERVER.";dbname=".DATABASE.";charset=utf8", USERNAME, PASSWORD);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $ex){
+    echo $ex->getMessage();
+}
+
+function executeQuery($query){
+    global $conn;
+    return $conn->query($query)->fetchAll();
+}
+
