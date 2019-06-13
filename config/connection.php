a8796908cb74b0aac94edf63b03018a5fb1cbff4
tomica95 <45568534+tomica95@users.noreply.github.com>
2019-06-13 21:37:53 +0200

    access for pages in log file

--- config/connection.php
+++ config/connection.php
@@ -2,6 +2,8 @@
 
 require_once "config.php";
 try {
+
+    writeAccess();
     
     $conn = new PDO("mysql:host=".SERVER.";dbname=".DATABASE.";charset=utf8", USERNAME, PASSWORD);
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
@@ -14,4 +16,15 @@ catch(PDOPDOException $ex){
 function executeQuery($query){
     global $conn;
     return $conn->query($query)->fetchAll();
+}
+
+function writeAccess(){
+
+    $file = fopen(BASE_URL ."data/log.txt","a");
+
+    $text = date("d.m.Y H:i:s") . "\t" ."Page:". basename($_SERVER['REQUEST_URI']) . "\t" ."Ip adress". $_SERVER['REMOTE_ADDR'] . "\n";
+
+    fwrite($file, $text);
+    fclose($file);
+
 }
